f618edc23ad2e829f5d9fb02c55c6cabbc60fc82
Ignaciofabian93 <ignaciofabian93@gmail.com>
2026-05-20 16:23:13 -0400

    fix

--- features/auth/screens/Login.tsx
+++ features/auth/screens/Login.tsx
@@ -1,6 +1,7 @@
 import { EkoruLogo } from "@/components/EkoruLogo/EkoruLogo";
 import { type SupportedLanguage } from "@/constants/settings";
 import { DictionaryProvider } from "@/i18n/context";
+import { Suspense } from "react";
 
 import { getAuthDictionary, NAMESPACE } from "../i18n";
 import { AuthShell } from "../ui/AuthShell";
@@ -17,7 +18,9 @@ export async function Login({ lang }: { lang: SupportedLanguage }) {
         subtitleKey="page.loginSubtitle"
         footer={{ textKey: "actions.noAccount", linkKey: "actions.signUp", href: `/${lang}/register` }}
       >
-        <LoginForm />
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </AuthShell>
     </DictionaryProvider>
   );
